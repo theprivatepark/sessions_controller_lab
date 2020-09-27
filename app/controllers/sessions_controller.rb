e62c6e860b37548ac99f318c28b7d94b088554ff
theprivatepark <anneypark@gmail.com>
2020-09-27 15:14:13 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+
+  def new
+  end
+
+  def create
+    if params[:name] == nil || params[:name] == ""
+      redirect_to login_path
+    else 
+      session[:name] = params[:name]
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to root_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'application#hello' #homepage, first thing user sees, where they login
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
